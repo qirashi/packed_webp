b4ec7967e16eb0748feab8aa8388f622e53e5e14
Qirashi <sartakovanton163@gmail.com>
2025-09-03 18:32:08 +0400

    build optimize

--- _build_.bat
+++ _build_.bat
@@ -6,24 +6,11 @@ echo off
 chcp 65001
 
 echo Начинаю сборку .exe
-go build -o ./riff.exe -ldflags="-s -w -buildid=" -trimpath -buildmode=exe -tags=release -asmflags="-trimpath" -mod=readonly ^
-main_logic.go main.go
+go build -o ./riff.exe -buildvcs=false -ldflags="-s -w -buildid=" -trimpath -buildmode=exe -tags=release -asmflags="-trimpath" -mod=readonly main_logic.go main.go
 if %ERRORLEVEL% neq 0 (
     echo Ошибка: Сборка завершилась с ошибкой. Код ошибки: %ERRORLEVEL%
     exit /b %ERRORLEVEL%
 )
 echo .exe Успешно собран.
-
-set TOOL1=%cd%\res\upx.exe
-if defined UPX (
-    set "UPX="
-)
-if exist "%TOOL1%" (
-    echo UPX найден, выполняю команды...
-    "%TOOL1%" -9 "%cd%\riff.exe"
-) else (
-    echo Ошибка: UPX не найден по пути "%TOOL1%".
-	echo Exe не сжат UPX.
-)
 
 @pause
